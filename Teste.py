39e08a8f92198d0e26fed023749aad662daedd9a
João Paulo Biazotto <biazottoj03@gmail.com>
2020-05-25 19:35:15 -0300

    Configuring Teste.py

--- Teste.py
+++ Teste.py
@@ -4,6 +4,4 @@ from Graph import createCircuit
 edgesNumber = ['1 2', '3 1', '1 5', '1 6', '2 3','2 6', '2 5', '3 6', '3 4','4 5', '5 6']
 edgesChar = ['A B', 'C A', 'A E', 'A F', 'B C','B F', 'B E', 'C F', 'C D','D E', 'E F']
 
-edges2 = ['1 2', '1 3', '2 3', '2 4']
-
-print(createCircuit(edges2, '1'))
+print(createCircuit(edgesChar, 'A'))
